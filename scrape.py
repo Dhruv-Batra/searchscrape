66e4a23a32cb7780c0750243a98e85270b5f08a1
Dhruv Batra <dhruvbatra2@gmail.com>
2021-10-21 11:55:08 -0400

    save files with unique filenames to specific directory and craigslist experimentation

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 from helper import parseSearch, exportData
+from datetime import datetime
 
 # input parameters and parse to transform into acceptable input
 OgSearchTerm = "iphone 6"
@@ -25,4 +26,16 @@ for items in soup.find_all('li', class_='s-item'):
     n+=1
 
 print(df.head())
-df.to_excel('ebay_phones.xlsx')
+
+# datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%H_%M_%S=%d_%m_%Y")
+
+# CHANGE ME TO THE FILEPATH TO YOUR RESULTS DIRECTORY
+filepath="C:/Users/dhruv/Documents/searchscrape/Results/"
+
+# add search term and datetime to filename to create unique, descriptive filename
+filepath+=str(OgSearchTerm+'='+dt_string+'.xlsx')
+
+# create excel file
+df.to_excel(filepath, index = False)
